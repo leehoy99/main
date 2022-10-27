c9039bcce7ae189b692cf15b2fd9f7e54b845423
leehoy99 <96473508+leehoy99@users.noreply.github.com>
2022-10-27 18:40:49 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -8,30 +8,17 @@ $(window).on('load', function() {
     }, 1000);
     setTimeout(function(){
         $(function() {
-            $('.main .main-img-box').mouseover(function(){
-                $('.main .main-img').css('animationPlayState','paused')
+
+            // main--------------------------------------------------------
+
+            $('.main .mokup-img').mouseover(function(){
+                $('.main .bg-img').css('animationPlayState','running')
             })
-            $('.main .main-img-box').mouseleave(function(){
-                $('.main .main-img').css('animationPlayState','running')
+            $('.main .mokup-img').mouseleave(function(){
+                $('.main .bg-img').css('animationPlayState','pause')
             })
-            $('.hover').hover(
-                function() {
-                $('.main .bg-img').addClass('down-move')
-                $('.main .bg-img.down-move').css('animationPlayState','running')
-                $('.main .hover div').addClass('main-hover')
-                $('.main .hover div p').addClass('main-hover')
-            }
-            ,
-                function() {
-                    $('.main .bg-img.down-move').css('animationPlayState','paused')
-                    $('.main .hover div').removeClass('main-hover')
-                    $('.main .hover div p').removeClass('main-hover')
-                    
-            }
-            )
-        
-            
-        
+            // grid-------------------------------------------------------
+
             $('.grid-contents').hover(function() {
                 $(this).addClass('grid-contents-hover')
                 $(this).find('.grid-text').children('.title').addClass('grid-contents-hover')
@@ -59,66 +46,50 @@ $(window).on('load', function() {
                 $('html').animate({scrollTop: distance - 100},600);
                 // console.log(distance)
             })
-        
-            // $('.grid-contents').hover(function() {
-            //     $(this).children(1).css({'color':'var(--sub-color)'},{'transition': 'color .3s' })
-            // }, function() {
-            //     $(this).children(1).css({'color':'var(--main-color)'},{'transition': 'color .3s' })
-            // })
-        
-        
-        
+
+            // section-------------------------------------------------------
+
             $(window).scroll(function() {
-                const t = $(window).scrollTop()
-                h= $(window).height();
-                if(h <= 910) {h=910}
-                else if (h >= 1100) {h=1100}
-                else {
-                    h= $(window).height();
+                const sct = $(window).scrollTop();
+                const h = $(window).height();
+                function sectionOffset(section) {
+                    var secOff = $(section).offset();
+                    return secOff.top;
                 }
-        
-                if(t > $('.grid').offset().top - 800) {
+
+                if (sct > sectionOffset('.grid')- 800) {
                     $('.grid').addClass('moving');
                 }
-                
-                var page1 = $('.section').eq(0).offset().top
-                var page2 = $('.section').eq(1).offset().top
-                var page3 = $('.section').eq(2).offset().top
-                var page4 = $('.section').eq(3).offset().top
-        
-                // console.log(page1)
-                // console.log($(window).scrollTop())
-        
-        
-                if(t > page1 - 800) {
-                    $('.section').eq(0).children('.box1').addClass('moving')
-                    $('.section').eq(0).children('.box2').addClass('moving')
+                if (sct > sectionOffset('.section1')- 800) {
+                    $('.section1').children('.box1').addClass('moving')
+                    $('.section1').children('.box2').addClass('moving')
                 }
-                if(t > page2 - 800) {
-                    $('.section').eq(1).children('.box1').addClass('moving')
-                    $('.section').eq(1).children('.box2').addClass('moving')
+                if (sct > sectionOffset('.section2')- 800) {
+                    $('.section2').children('.box1').addClass('moving')
+                    $('.section2').children('.box2').addClass('moving')
                 }
-                if(t > page3 - 800) {
-                    $('.section').eq(2).children('.box1').addClass('moving')
-                    $('.section').eq(2).children('.box2').addClass('moving')
+                if (sct > sectionOffset('.section3')- 800) {
+                    $('.section3').children('.box1').addClass('moving')
+                    $('.section3').children('.box2').addClass('moving')
                 }
-                if(t > page4 - 800 ) {
-                    $('.section').eq(3).children('.box').addClass('moving')
+                if (sct > sectionOffset('.section4')- 800) {
+                    $('.section4').children('.box1').addClass('moving')
+                    $('.section4').children('.box2').addClass('moving')
                 }
-        
-                if(t <= h-200) {
+
+                if(sct <= h-200) {
                     $('nav').removeClass('move');
                     $('.nav-box > ul > li p').removeClass('move');
                 }
-                if(t > h-200) {
+                if(sct > h-200) {
                     $('nav').addClass('move');
                     $('.nav-box > ul > li p').addClass('move');
                 } 
-                if (t < h) {
+                if (sct < h) {
                     $('nav').removeClass('down');
                     $('.nav-box > ul > li p').removeClass('down');
                 }
-                if (t >= h-170) {
+                if (sct >= h-170) {
                     $('nav').addClass('down');
                     $('.nav-box > ul > li p').addClass('down');
                 } 
@@ -126,4 +97,3 @@ $(window).on('load', function() {
         })
     }, 1200);
 });
-
